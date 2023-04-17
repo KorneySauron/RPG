80580d02083f5de06c415f325f808202c2bbb017
Korney Saushkin <saukorn03@gmail.com>
2023-04-17 20:33:09 +0200

    Add starting menu template

---
+++ Colors.java
@@ -0,0 +1,10 @@
+public interface Colors {
+        String SPACE = "\u2009\u2009";
+        String RESET = "\u001B[0m";
+        String RED = "\u001B[31m";
+        String GREEN = "\u001B[32m";
+        String YELLOW = "\u001B[33m";
+        String BLUE = "\u001B[34m";
+        String PURPLE = "\u001B[35m";
+        String CYAN = "\u001B[36m";
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Colors.BLUE + "Добро пожаловать в игру " + Colors.GREEN + "\"Древние земли\"" + Colors.RESET);
+        System.out.println("""
+                Выберите действие:
+                1.Новая игра
+                2.Продолжить
+                3.Выйти из игры""");
+        switch (Integer.parseInt(br.readLine())) {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+        }
     }
 }
